0c87dbdc8c4351bbde9c587a43a5b4601d678997
Conzcode <52662469+Conzcode@users.noreply.github.com>
2025-09-01 15:16:54 -0500

    Create tasks

---
+++ MD-Orient-S/MD-Orient-S-trials.js
@@ -0,0 +1,605 @@
+prac_trials = [
+  {
+    "num_trial": 1,
+    "set_size": 6,
+    "target_mean_level": 75,
+    "sd_level": 9,
+    "distract_mean_level": 105,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "PracStimArray01.png"
+  },
+  {
+    "num_trial": 2,
+    "set_size": 6,
+    "target_mean_level": 105,
+    "sd_level": 9,
+    "distract_mean_level": 75,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "PracStimArray02.png"
+  },
+  {
+    "num_trial": 3,
+    "set_size": 8,
+    "target_mean_level": 75,
+    "sd_level": 9,
+    "distract_mean_level": 105,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "PracStimArray03.png"
+  },
+  {
+    "num_trial": 4,
+    "set_size": 8,
+    "target_mean_level": 105,
+    "sd_level": 9,
+    "distract_mean_level": 75,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "PracStimArray04.png"
+  },
+  {
+    "num_trial": 5,
+    "set_size": 12,
+    "target_mean_level": 75,
+    "sd_level": 9,
+    "distract_mean_level": 105,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "PracStimArray05.png"
+  },
+  {
+    "num_trial": 6,
+    "set_size": 12,
+    "target_mean_level": 105,
+    "sd_level": 9,
+    "distract_mean_level": 75,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "PracStimArray06.png"
+  }
+]
+
+trials = [
+  {
+    "num_trial": 1,
+    "set_size": 6,
+    "target_mean_level": 87,
+    "sd_level": 9,
+    "distract_mean_level": 93,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray01.png"
+  },
+  {
+    "num_trial": 2,
+    "set_size": 8,
+    "target_mean_level": 75,
+    "sd_level": 11,
+    "distract_mean_level": 105,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray02.png"
+  },
+  {
+    "num_trial": 3,
+    "set_size": 12,
+    "target_mean_level": 87,
+    "sd_level": 13,
+    "distract_mean_level": 93,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray03.png"
+  },
+  {
+    "num_trial": 4,
+    "set_size": 8,
+    "target_mean_level": 87,
+    "sd_level": 11,
+    "distract_mean_level": 93,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray04.png"
+  },
+  {
+    "num_trial": 5,
+    "set_size": 12,
+    "target_mean_level": 75,
+    "sd_level": 11,
+    "distract_mean_level": 105,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray05.png"
+  },
+  {
+    "num_trial": 6,
+    "set_size": 6,
+    "target_mean_level": 81,
+    "sd_level": 9,
+    "distract_mean_level": 99,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray06.png"
+  },
+  {
+    "num_trial": 7,
+    "set_size": 6,
+    "target_mean_level": 81,
+    "sd_level": 11,
+    "distract_mean_level": 99,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray07.png"
+  },
+  {
+    "num_trial": 8,
+    "set_size": 6,
+    "target_mean_level": 99,
+    "sd_level": 11,
+    "distract_mean_level": 81,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray08.png"
+  },
+  {
+    "num_trial": 9,
+    "set_size": 6,
+    "target_mean_level": 105,
+    "sd_level": 9,
+    "distract_mean_level": 75,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray09.png"
+  },
+  {
+    "num_trial": 10,
+    "set_size": 6,
+    "target_mean_level": 105,
+    "sd_level": 13,
+    "distract_mean_level": 75,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray10.png"
+  },
+  {
+    "num_trial": 11,
+    "set_size": 8,
+    "target_mean_level": 87,
+    "sd_level": 9,
+    "distract_mean_level": 93,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray11.png"
+  },
+  {
+    "num_trial": 12,
+    "set_size": 12,
+    "target_mean_level": 99,
+    "sd_level": 13,
+    "distract_mean_level": 81,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray12.png"
+  },
+  {
+    "num_trial": 13,
+    "set_size": 8,
+    "target_mean_level": 93,
+    "sd_level": 9,
+    "distract_mean_level": 87,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray13.png"
+  },
+  {
+    "num_trial": 14,
+    "set_size": 8,
+    "target_mean_level": 81,
+    "sd_level": 9,
+    "distract_mean_level": 99,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray14.png"
+  },
+  {
+    "num_trial": 15,
+    "set_size": 12,
+    "target_mean_level": 99,
+    "sd_level": 11,
+    "distract_mean_level": 81,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray15.png"
+  },
+  {
+    "num_trial": 16,
+    "set_size": 12,
+    "target_mean_level": 81,
+    "sd_level": 11,
+    "distract_mean_level": 99,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray16.png"
+  },
+  {
+    "num_trial": 17,
+    "set_size": 12,
+    "target_mean_level": 81,
+    "sd_level": 9,
+    "distract_mean_level": 99,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray17.png"
+  },
+  {
+    "num_trial": 18,
+    "set_size": 8,
+    "target_mean_level": 81,
+    "sd_level": 11,
+    "distract_mean_level": 99,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray18.png"
+  },
+  {
+    "num_trial": 19,
+    "set_size": 6,
+    "target_mean_level": 75,
+    "sd_level": 9,
+    "distract_mean_level": 105,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray19.png"
+  },
+  {
+    "num_trial": 20,
+    "set_size": 6,
+    "target_mean_level": 105,
+    "sd_level": 11,
+    "distract_mean_level": 75,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray20.png"
+  },
+  {
+    "num_trial": 21,
+    "set_size": 6,
+    "target_mean_level": 99,
+    "sd_level": 13,
+    "distract_mean_level": 81,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray21.png"
+  },
+  {
+    "num_trial": 22,
+    "set_size": 8,
+    "target_mean_level": 81,
+    "sd_level": 13,
+    "distract_mean_level": 99,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray22.png"
+  },
+  {
+    "num_trial": 23,
+    "set_size": 8,
+    "target_mean_level": 93,
+    "sd_level": 11,
+    "distract_mean_level": 87,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray23.png"
+  },
+  {
+    "num_trial": 24,
+    "set_size": 8,
+    "target_mean_level": 75,
+    "sd_level": 9,
+    "distract_mean_level": 105,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray24.png"
+  },
+  {
+    "num_trial": 25,
+    "set_size": 12,
+    "target_mean_level": 105,
+    "sd_level": 13,
+    "distract_mean_level": 75,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray25.png"
+  },
+  {
+    "num_trial": 26,
+    "set_size": 6,
+    "target_mean_level": 99,
+    "sd_level": 9,
+    "distract_mean_level": 81,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray26.png"
+  },
+  {
+    "num_trial": 27,
+    "set_size": 12,
+    "target_mean_level": 81,
+    "sd_level": 13,
+    "distract_mean_level": 99,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray27.png"
+  },
+  {
+    "num_trial": 28,
+    "set_size": 12,
+    "target_mean_level": 93,
+    "sd_level": 11,
+    "distract_mean_level": 87,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray28.png"
+  },
+  {
+    "num_trial": 29,
+    "set_size": 8,
+    "target_mean_level": 87,
+    "sd_level": 13,
+    "distract_mean_level": 93,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray29.png"
+  },
+  {
+    "num_trial": 30,
+    "set_size": 6,
+    "target_mean_level": 93,
+    "sd_level": 11,
+    "distract_mean_level": 87,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray30.png"
+  },
+  {
+    "num_trial": 31,
+    "set_size": 6,
+    "target_mean_level": 81,
+    "sd_level": 13,
+    "distract_mean_level": 99,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray31.png"
+  },
+  {
+    "num_trial": 32,
+    "set_size": 6,
+    "target_mean_level": 93,
+    "sd_level": 13,
+    "distract_mean_level": 87,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray32.png"
+  },
+  {
+    "num_trial": 33,
+    "set_size": 6,
+    "target_mean_level": 87,
+    "sd_level": 13,
+    "distract_mean_level": 93,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray33.png"
+  },
+  {
+    "num_trial": 34,
+    "set_size": 6,
+    "target_mean_level": 93,
+    "sd_level": 9,
+    "distract_mean_level": 87,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray34.png"
+  },
+  {
+    "num_trial": 35,
+    "set_size": 12,
+    "target_mean_level": 87,
+    "sd_level": 9,
+    "distract_mean_level": 93,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray35.png"
+  },
+  {
+    "num_trial": 36,
+    "set_size": 8,
+    "target_mean_level": 75,
+    "sd_level": 13,
+    "distract_mean_level": 105,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray36.png"
+  },
+  {
+    "num_trial": 37,
+    "set_size": 6,
+    "target_mean_level": 75,
+    "sd_level": 13,
+    "distract_mean_level": 105,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray37.png"
+  },
+  {
+    "num_trial": 38,
+    "set_size": 8,
+    "target_mean_level": 99,
+    "sd_level": 13,
+    "distract_mean_level": 81,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray38.png"
+  },
+  {
+    "num_trial": 39,
+    "set_size": 12,
+    "target_mean_level": 87,
+    "sd_level": 11,
+    "distract_mean_level": 93,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray39.png"
+  },
+  {
+    "num_trial": 40,
+    "set_size": 12,
+    "target_mean_level": 99,
+    "sd_level": 9,
+    "distract_mean_level": 81,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray40.png"
+  },
+  {
+    "num_trial": 41,
+    "set_size": 12,
+    "target_mean_level": 93,
+    "sd_level": 13,
+    "distract_mean_level": 87,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray41.png"
+  },
+  {
+    "num_trial": 42,
+    "set_size": 6,
+    "target_mean_level": 87,
+    "sd_level": 11,
+    "distract_mean_level": 93,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray42.png"
+  },
+  {
+    "num_trial": 43,
+    "set_size": 12,
+    "target_mean_level": 105,
+    "sd_level": 9,
+    "distract_mean_level": 75,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray43.png"
+  },
+  {
+    "num_trial": 44,
+    "set_size": 8,
+    "target_mean_level": 99,
+    "sd_level": 9,
+    "distract_mean_level": 81,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray44.png"
+  },
+  {
+    "num_trial": 45,
+    "set_size": 8,
+    "target_mean_level": 99,
+    "sd_level": 11,
+    "distract_mean_level": 81,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray45.png"
+  },
+  {
+    "num_trial": 46,
+    "set_size": 8,
+    "target_mean_level": 105,
+    "sd_level": 9,
+    "distract_mean_level": 75,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray46.png"
+  },
+  {
+    "num_trial": 47,
+    "set_size": 8,
+    "target_mean_level": 105,
+    "sd_level": 11,
+    "distract_mean_level": 75,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray47.png"
+  },
+  {
+    "num_trial": 48,
+    "set_size": 12,
+    "target_mean_level": 75,
+    "sd_level": 13,
+    "distract_mean_level": 105,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray48.png"
+  },
+  {
+    "num_trial": 49,
+    "set_size": 12,
+    "target_mean_level": 105,
+    "sd_level": 11,
+    "distract_mean_level": 75,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray49.png"
+  },
+  {
+    "num_trial": 50,
+    "set_size": 12,
+    "target_mean_level": 75,
+    "sd_level": 9,
+    "distract_mean_level": 105,
+    "cue_condition": "bottom",
+    "correct_answer": "f",
+    "stim_img": "StimArray50.png"
+  },
+  {
+    "num_trial": 51,
+    "set_size": 6,
+    "target_mean_level": 75,
+    "sd_level": 11,
+    "distract_mean_level": 105,
+    "cue_condition": "top",
+    "correct_answer": "f",
+    "stim_img": "StimArray51.png"
+  },
+  {
+    "num_trial": 52,
+    "set_size": 8,
+    "target_mean_level": 93,
+    "sd_level": 13,
+    "distract_mean_level": 87,
+    "cue_condition": "bottom",
+    "correct_answer": "j",
+    "stim_img": "StimArray52.png"
+  },
+  {
+    "num_trial": 53,
+    "set_size": 12,
+    "target_mean_level": 93,
+    "sd_level": 9,
+    "distract_mean_level": 87,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray53.png"
+  },
+  {
+    "num_trial": 54,
+    "set_size": 8,
+    "target_mean_level": 105,
+    "sd_level": 13,
+    "distract_mean_level": 75,
+    "cue_condition": "top",
+    "correct_answer": "j",
+    "stim_img": "StimArray54.png"
+  }
+]
